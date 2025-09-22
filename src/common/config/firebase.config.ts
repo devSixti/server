67cc0674959ad45c8e803445ae2a3b72785b3891
desxisti <desxisti@gmai.com>
2025-09-22 18:25:16 -0500

    Resolving Bug Firebase

--- src/common/config/firebase.config.ts
+++ src/common/config/firebase.config.ts
@@ -1,9 +1,15 @@
-import  admin  from 'firebase-admin';
+import admin from 'firebase-admin';
+import dotenv from 'dotenv';
 
-const serviceAccount = require('../keys/firebase.services.json');
+dotenv.config({ path: '.env.dev' });
+
+const config = JSON.parse(process.env.FIREBASE_CONFIG!);
+
+// Reemplaza literal \\n por salto de línea real
+config.private_key = config.private_key.replace(/\\n/g, '\n');
 
 export const firebaseAdmin = admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert(config),
 });
 
-console.log("Firebase ok.")
+console.log("Firebase initialized successfully");
